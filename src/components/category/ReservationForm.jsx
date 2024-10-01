dc1c2e3eb32556b46d8dfdb1353decb8e0d01365
apsanregmi <mahesh.regmi@cloudproai.com>
2024-10-01 13:04:51 +0545

    gallery images added

--- src/components/category/ReservationForm.jsx
+++ src/components/category/ReservationForm.jsx
@@ -1,8 +1,51 @@
 import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 function ReservationForm() {
-  const [startDate1, setStartDate1] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    eventDate: null,
+    eventStartTime: "",
+    numberOfGuests: "",
+    typeOfEvent: "",
+    additionalComments: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleDateChange = (date) => {
+    setFormData({ ...formData, eventDate: date });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Integrate EmailJS here later for sending form data
+    console.log("Form Data: ", formData);
+    alert("Reservation submitted successfully!");
+    // Reset form
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      eventDate: null,
+      eventStartTime: "",
+      numberOfGuests: "",
+      typeOfEvent: "",
+      additionalComments: "",
+    });
+  };
+
   return (
     <div className="container">
       <div className="reservation-1 mb-120 mt-120">
@@ -15,7 +58,7 @@ function ReservationForm() {
                   src="assets/images/icon/sub-title-vec.svg"
                   alt="sub-title-vec"
                 />
-                Online Reserve
+                Online Reservation
                 <img
                   className="right-vec"
                   src="assets/images/icon/sub-title-vec.svg"
@@ -28,62 +71,136 @@ function ReservationForm() {
         </div>
         <div className="row justify-content-center">
           <div className="col-lg-10">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="row justify-content-center">
+                {/* First Name */}
                 <div className="col-lg-6 col-md-6 mb-25">
                   <div className="form-inner">
-                    <input type="text" placeholder="Name*" />
-                  </div>
-                </div>
-                <div className="col-lg-6 col-md-6 mb-25">
-                  <div className="form-inner">
-                    <input type="text" placeholder="Phone*" />
-                  </div>
-                </div>
-                <div className="col-lg-6 col-md-6 mb-25">
-                  <div className="form-inner">
-                    <input type="text" placeholder="People" />
-                  </div>
-                </div>
-                <div className="col-lg-6 col-md-6 mb-25">
-                  <div className="form-inner date-icon">
-                    <ReactDatePicker
-                      selected={startDate1}
-                      onChange={(date) => setStartDate1(date)}
-                      placeholderText="Check In"
-                      className="claender"
+                    <input
+                      type="text"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      placeholder="First Name*"
+                      required
                     />
                   </div>
                 </div>
+                
+                {/* Last Name */}
                 <div className="col-lg-6 col-md-6 mb-25">
                   <div className="form-inner">
-                    <select className="time-select">
-                      <option value="time">08 : 00 am</option>
-                      <option>09 : 00 am</option>
-                      <option value={1}>10 : 00 am</option>
-                      <option value={2}>11 : 00 am</option>
-                      <option value={3}>12 : 00 pm</option>
-                      <option value={4}>01 : 00 pm</option>
-                      <option value={5}>02 : 00 pm</option>
-                      <option value={6}>03 : 00 pm</option>
-                      <option value={7}>04 : 00 pm</option>
-                      <option value={8}>05 : 00 pm</option>
-                      <option value={9}>06 : 00 pm</option>
-                      <option value={10}>07 : 00 pm</option>
-                      <option value={11}>08 : 00 pm</option>
-                      <option value={12}>09 : 00 pm</option>
-                      <option value={13}>10 : 00 pm</option>
-                    </select>
+                    <input
+                      type="text"
+                      name="lastName"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      placeholder="Last Name*"
+                      required
+                    />
                   </div>
                 </div>
-                <div className="col-lg-6 col-md-6 sm-mb-25">
+                
+                {/* Email Address */}
+                <div className="col-lg-6 col-md-6 mb-25">
                   <div className="form-inner">
-                    <input type="email" placeholder="Email" />
+                    <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      placeholder="Email Address*"
+                      required
+                    />
                   </div>
                 </div>
+                
+                {/* Phone Number */}
+                <div className="col-lg-6 col-md-6 mb-25">
+                  <div className="form-inner">
+                    <input
+                      type="text"
+                      name="phoneNumber"
+                      value={formData.phoneNumber}
+                      onChange={handleChange}
+                      placeholder="Phone Number*"
+                      required
+                    />
+                  </div>
+                </div>
+                
+                {/* Event Date */}
+                <div className="col-lg-6 col-md-6 mb-25">
+                  <div className="form-inner date-icon">
+                    <ReactDatePicker
+                      selected={formData.eventDate}
+                      onChange={handleDateChange}
+                      placeholderText="Event Date*"
+                      className="claender"
+                      required
+                    />
+                  </div>
+                </div>
+
+                {/* Event Start Time */}
+                <div className="col-lg-6 col-md-6 mb-25">
+                  <div className="form-inner">
+                    <input
+                      type="time"
+                      name="eventStartTime"
+                      value={formData.eventStartTime}
+                      onChange={handleChange}
+                      placeholder="Event Start Time*"
+                      required
+                    />
+                  </div>
+                </div>
+                
+                {/* Number of Guests Attending */}
+                <div className="col-lg-6 col-md-6 mb-25">
+                  <div className="form-inner">
+                    <input
+                      type="number"
+                      name="numberOfGuests"
+                      value={formData.numberOfGuests}
+                      onChange={handleChange}
+                      placeholder="Number of Guests*"
+                      required
+                    />
+                  </div>
+                </div>
+                
+                {/* Type of Event */}
+                <div className="col-lg-6 col-md-6 mb-25">
+                  <div className="form-inner">
+                    <input
+                      type="text"
+                      name="typeOfEvent"
+                      value={formData.typeOfEvent}
+                      onChange={handleChange}
+                      placeholder="Type of Event*"
+                      required
+                    />
+                  </div>
+                </div>
+                
+                {/* Additional Comments */}
+                <div className="col-lg-12 mb-25">
+                  <div className="form-inner">
+                    <textarea
+                      name="additionalComments"
+                      value={formData.additionalComments}
+                      onChange={handleChange}
+                      placeholder="Additional Comments"
+                      rows={4}
+                    />
+                  </div>
+                </div>
+                
+                {/* Submit Button */}
                 <div className="col-lg-6 col-md-6">
                   <div className="form-inner">
-                    <button type="submit">Reserve Now</button>
+                    <button type="submit">Send Reservation</button>
                   </div>
                 </div>
               </div>
